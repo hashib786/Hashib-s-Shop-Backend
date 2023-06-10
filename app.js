7ea5d52d482669d2d86f8ab3213941bf6ff47a02
Hashib Raja <hashibraja23@gmail.com>
2023-06-10 20:13:53 +0530

    remove ErrorHandler don't need in app.js

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const ErrorHandler = require("./utils/ErrorHandler");
 const cookieParser = require("cookie-parser");
 const { urlencoded } = require("body-parser");
 const cors = require("cors");
