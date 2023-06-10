5810852d0f5ce23c80857ff08d5c608da31a8156
Hashib Raja <hashibraja23@gmail.com>
2023-06-10 20:29:43 +0530

    Update Frontend in our backend Cors

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://hashibs-shop.vercel.app/"],
     credentials: true,
   })
 );
